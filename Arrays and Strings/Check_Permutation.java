d590fa9e13f728bf1b16e8d36cb31fb9be818826
Noah Park <noahjinpark@gmail.com>
2020-02-02 16:02:49 -0600

    Update Check_Permutation.java

--- Arrays and Strings/Check_Permutation.java
+++ Arrays and Strings/Check_Permutation.java
@@ -1,3 +1,13 @@
+/* 
+
+Noah Park
+
+Problem:
+
+Given two strings, write a method to decide if one is a permutation of the other
+
+*/
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Hashtable;
