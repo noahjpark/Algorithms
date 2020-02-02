561cc1b76f332db4014cc3bb9e7b94032ea167f1
Noah Park <noahjinpark@gmail.com>
2020-02-02 16:05:20 -0600

    Update Check_Permutation.java

--- Arrays and Strings/Check_Permutation.java
+++ Arrays and Strings/Check_Permutation.java
@@ -4,7 +4,7 @@ Noah Park
 
 Problem:
 
-Given two strings, write a method to decide if one is a permutation of the other
+Given two strings, write a method to decide if one is a permutation of the other.
 
 */
 
