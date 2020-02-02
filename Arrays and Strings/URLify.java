e559354c360531dc410fde4416a0905cb38e9b18
Noah Park <noahjinpark@gmail.com>
2020-02-02 16:04:52 -0600

    Update URLify.java

--- Arrays and Strings/URLify.java
+++ Arrays and Strings/URLify.java
@@ -1,3 +1,14 @@
+/*
+
+Noah Park
+
+Problem:
+
+Write a method to replace all spaces in a string with '%20'. You may assume that the string has sufficient space at the end to hold the
+additional characters, and that you are given the "true" length of the string.
+
+*/
+
 public class URLify {
 
     // Time: O(n)
