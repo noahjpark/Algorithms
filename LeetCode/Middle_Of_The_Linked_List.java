4213c31ce61f3bea94cd15aa80bb5995635f5b15
Noah Park <noahjinpark@gmail.com>
2020-11-03 20:59:17 -0600

    Create Middle_Of_The_Linked_List.java

---
+++ LeetCode/Middle_Of_The_Linked_List.java
@@ -0,0 +1,30 @@
+/* Noah Park
+
+Given a non-empty, singly linked list with head node head, return a middle node of linked list.
+
+If there are two middle nodes, return the second middle node.
+
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return slow;
+    }
+}
