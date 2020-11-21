f41ea2b690ec57be5e441c17d9e476e10a308552
Noah Park <noahjinpark@gmail.com>
2020-11-21 16:39:05 -0600

    Create Slowest_Key.java

---
+++ LeetCode/Slowest_Key.java
@@ -0,0 +1,33 @@
+/* Noah Park
+
+A newly designed keypad was tested, where a tester pressed a sequence of n keys, one at a time.
+
+You are given a string keysPressed of length n, where keysPressed[i] was the ith key pressed in the testing sequence, and a sorted list releaseTimes, where releaseTimes[i] was the time the ith key was released. Both arrays are 0-indexed. The 0th key was pressed at the time 0, and every subsequent key was pressed at the exact time the previous key was released.
+
+The tester wants to know the key of the keypress that had the longest duration. The ith keypress had a duration of releaseTimes[i] - releaseTimes[i - 1], and the 0th keypress had a duration of releaseTimes[0].
+
+Note that the same key could have been pressed multiple times during the test, and these multiple presses of the same key may not have had the same duration.
+
+Return the key of the keypress that had the longest duration. If there are multiple such keypresses, return the lexicographically largest key of the keypresses.
+
+*/
+
+class Solution {
+    public char slowestKey(int[] releaseTimes, String keysPressed) {
+        // prev for the prev value, max for the max length press
+        int prev = releaseTimes[0], max = releaseTimes[0];
+        char key = keysPressed.charAt(0); // character that has been pressed the longest
+        int n = releaseTimes.length; // length of the array
+        for(int i = 1; i < n; i++){
+            char newkey = keysPressed.charAt(i); // current character
+            int val = releaseTimes[i] - prev; // store the press length
+            prev = releaseTimes[i]; // update prev
+            releaseTimes[i] = val; // update the array
+            // if there is a new max, or if there is an equal max with a larger lexicographical key, update the max and key
+            if(max < releaseTimes[i] || (max == releaseTimes[i] && newkey > key)) { max = releaseTimes[i]; key = newkey; }
+        }
+        
+        // return the longest pressed key (or largest lexicographically if there are two)
+        return key;
+    }
+}
