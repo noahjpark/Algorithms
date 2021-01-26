ec10591854017d22395b2a9d058671fa0288ee8f
Noah Park <noahjinpark@gmail.com>
2021-01-26 14:40:55 -0600

    Create Find_The_Highest_Altitude.java

---
+++ LeetCode/Find_The_Highest_Altitude.java
@@ -0,0 +1,22 @@
+/* Noah Park
+
+There is a biker going on a road trip. The road trip consists of n + 1 points at different altitudes. The biker starts his trip on point 0 with altitude equal 0.
+
+You are given an integer array gain of length n where gain[i] is the net gain in altitude between points i​​​​​​ and i + 1 for all (0 <= i < n). Return the highest altitude of a point.
+
+*/
+
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0, cur = 0; // max is max altitude, cur is the current altitude
+        
+        // update current and max altitude at each point
+        for (int alt : gain) {
+            cur += alt;
+            max = Math.max(max, cur);
+        }
+        
+        // return the max altitude
+        return max;
+    }
+}
