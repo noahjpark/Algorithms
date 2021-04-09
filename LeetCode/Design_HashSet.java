71a5d33b742382948c45b555b51bc86938fb17e4
Noah Park <noahjinpark@gmail.com>
2021-04-08 23:03:41 -0500

    Create Design_HashSet.java

---
+++ LeetCode/Design_HashSet.java
@@ -0,0 +1,45 @@
+/* Noah Park
+
+Design a HashSet without using any built-in hash table libraries.
+
+Implement MyHashSet class:
+
+void add(key) Inserts the value key into the HashSet.
+bool contains(key) Returns whether the value key exists in the HashSet or not.
+void remove(key) Removes the value key in the HashSet. If key does not exist in the HashSet, do nothing.
+
+*/
+
+class MyHashSet {
+    
+    // Intuition: Since we can have a key from 0 through 1000000, we use an array of this size and apply counting sort.
+    // Time: O(1)
+    // Space: O(1)
+    boolean[] set;
+
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        set = new boolean[1000001];
+    }
+    
+    public void add(int key) {
+        set[key] = true;
+    }
+    
+    public void remove(int key) {
+        set[key] = false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return set[key];
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
