08ebfa325e12cfb7a4784c3eea2b392a38e57d8e
Noah Park <noahjinpark@gmail.com>
2021-05-30 20:47:27 -0500

    Create Print_In_Order.java

---
+++ LeetCode/Print_In_Order.java
@@ -0,0 +1,47 @@
+/* Noah Park
+
+Suppose we have a class:
+
+public class Foo {
+  public void first() { print("first"); }
+  public void second() { print("second"); }
+  public void third() { print("third"); }
+}
+The same instance of Foo will be passed to three different threads. Thread A will call first(), thread B will call second(), and thread C will call third(). Design a mechanism and modify the program to ensure that second() is executed after first(), and third() is executed after second().
+
+Note:
+
+We do not know how the threads will be scheduled in the operating system, even though the numbers in the input seem to imply the ordering. The input format you see is mainly to ensure our tests' comprehensiveness.
+
+*/
+
+class Foo {
+
+    // Maintain semaphores to solve the concurrency problem.
+    Semaphore s1, s2;
+    
+    public Foo() {
+        s1 = new Semaphore(0);
+        s2 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        s1.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        s1.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        s2.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        s2.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        s2.release();
+    }
+}
